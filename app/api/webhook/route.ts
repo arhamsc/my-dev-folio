daa540fab1e7f9590b526dc16a4f42ef211f36a5
arhamsc <shaik.arham@gmail.com>
2024-01-21 02:05:21 +0530

    Route

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -63,7 +63,7 @@ export async function POST(req: Request) {
       name: `${first_name}${last_name ? ` ${last_name}` : ""}`,
       email: email_addresses[0].email_address,
       picture: image_url,
-      username: username!,
+      username: username ?? "",
     });
 
     return NextResponse.json({ message: "OK", user: mongoUser });
@@ -77,7 +77,7 @@ export async function POST(req: Request) {
         name: `${first_name}${last_name ? ` ${last_name}` : ""}`,
         email: email_addresses[0].email_address,
         picture: image_url,
-        username: username!,
+        username: username ?? "",
       },
       path: `/profile/${id}`,
     });
